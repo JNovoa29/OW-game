55986706189ccb9f427ba9513f3081511b32ee76
jnovoa29 <jordan.novoa15@gmail.com>
2018-06-05 20:00:10 -0700

    restructure

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = props => {
+    return (
+    <div>
+    <nav class="navbar">
+        <span class="navbar-brand mb-0 h1">Overwatch Match Game</span>
+    </nav>
+</div>
+    )
+}
+
+
+export default Navbar;
